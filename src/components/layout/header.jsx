c9535868cbeab05c6fd8d30498c9736883ee6939
Dio <diogomiranda8091@gmail.com>
2025-10-15 09:25:55 -0500

    fix: update dependencies and add Tailwind CSS fix: Navbar and header components updated for better UX feat: integrate Tailwind CSS for improved styling

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -5,15 +5,7 @@ import Navbar from './Navbar';
 export default function Header() {
   return (
     <header className="App-header">
-      <div className="top-bar">
-        <div className="top-bar-content">
-          <div>
-            <input type="text" placeholder="Search..." />
-          </div>
-        </div>
-      </div>
       <div className="Header-content">
-        <div className="Logo"><Link to="/">AccelNet</Link></div>
         <Navbar />
       </div>
     </header>
