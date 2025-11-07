518958fec5628e126237792909b3b188ac2f3a09
Dio <diogomiranda8091@gmail.com>
2025-11-07 11:46:46 -0600

    update vite.config.js to use the correct React

---
+++ accelnet-frontend/src/pages/home/HomePage.tsx
@@ -0,0 +1,22 @@
+import { ResearchHero } from './componets/ResearchHero';
+import { ResearchFindings } from './componets/ResearchFindings';
+import { DataVisualization } from './componets/DataVisualization';
+import { ResearchMethodology } from './componets/ResearchMethodology';
+import { Publications } from './componets/Publications';
+import { ResearchTeam } from './componets/ResearchTeam';
+
+// We do NOT import the ResearchFooter, because your Layout.jsx already adds your site-wide footer.
+
+export default function HomePage() {
+  return (
+    // We use a React fragment <>...</> to avoid adding an extra <div>
+    <> 
+      <ResearchHero />
+      <ResearchFindings />
+      <DataVisualization />
+      <ResearchMethodology />
+      <Publications />
+      <ResearchTeam />
+    </>
+  );
+}
